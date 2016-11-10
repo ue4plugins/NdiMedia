e922ea6bc0a51f5fc533e5f11da35889bdb95b52
Gerke Max Preussner <info@headcrash.us>
2016-11-09 19:47:50 -0500

    Fixed compile error when compiling as project plug-in.
    https://github.com/ue4plugins/NdiMedia/issues/7

--- Source/NdiMediaEditor/Private/NdiMediaEditorPCH.h
+++ Source/NdiMediaEditor/Private/NdiMediaEditorPCH.h
@@ -2,18 +2,21 @@
 
 #pragma once
 
+#include "Runtime/Core/Public/Core.h"
+#include "Runtime/Core/Public/Modules/ModuleManager.h"
+#include "Runtime/CoreUObject/Public/CoreUObject.h"
+#include "Runtime/Slate/Public/SlateBasics.h"
+
 #include "Developer/AssetTools/Public/AssetTypeCategories.h"
 
+// begin missing dependencies in PropertyEditor
+#include "Editor/EditorStyle/Public/EditorStyle.h"
+// end missing dependencies
 #include "Editor/PropertyEditor/Public/DetailCategoryBuilder.h"
 #include "Editor/PropertyEditor/Public/DetailLayoutBuilder.h"
 #include "Editor/PropertyEditor/Public/DetailWidgetRow.h"
 #include "Editor/PropertyEditor/Public/IDetailPropertyRow.h"
 #include "Editor/PropertyEditor/Public/PropertyEditorModule.h"
-
-#include "Runtime/Core/Public/Core.h"
-#include "Runtime/Core/Public/Modules/ModuleManager.h"
-#include "Runtime/CoreUObject/Public/CoreUObject.h"
-#include "Runtime/Slate/Public/SlateBasics.h"
 
 #include "../../NdiMedia/Public/NdiMediaFinder.h"
 #include "../../NdiMedia/Public/NdiMediaSource.h"
